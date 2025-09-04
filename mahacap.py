7eaabc213d20781de7811e5e9f5d7415e757c02c
EinTrust <eintrusts@gmail.com>
2025-09-04 22:04:33 +0530

    Update mahacap.py

--- mahacap.py
+++ mahacap.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import os
 
 # ---------------------------
-# Page Confi
+# Page Configuration
 # ---------------------------
 st.set_page_config(page_title="Maharashtra CAP Dashboard", page_icon="🌍", layout="wide")
 
